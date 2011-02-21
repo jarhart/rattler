9b42992f60d828d747c34b699a6ae11567202d3e
jarhart <jarhart@gmail.com>
2011-02-21 12:46:29 -0800

    refactor recursive descent parser and add parse_fully methods

--- lib/rattler/runtime/parser.rb
+++ lib/rattler/runtime/parser.rb
@@ -15,7 +15,16 @@ module Rattler::Runtime
   # @author Jason Arhart
   #
   class Parser
-    
+
+    # Parse +source+ and raise a {SyntaxError} if the parse fails.
+    #
+    # @param (see #initialize)
+    # @raise (see #parse!)
+    # @return (see #parse!)
+    def self.parse!(source, options={})
+      self.new(source, options).parse!
+    end
+
     # Create a new parser to parse +source+.
     #
     # @param [String] source the source to parse
@@ -26,24 +35,58 @@ module Rattler::Runtime
       @scanner = StringScanner.new(source)
       @tab_size = options[:tab_size]
     end
-    
+
     # The source that this parser parses
     # @return [String] the source that this parser parses
     attr_reader :source
-    
+
+    # Parse or register a parse failure
+    #
+    # @return the parse result
+    def parse
+      catch(:parse_failed) { return finish __parse__ }
+      false
+    end
+
+    # Parse or raise a {SyntaxError}
+    #
+    # @raise [SyntaxError] a {SyntaxError} if the parse fails
+    #
+    # @return (see #parse)
+    def parse!
+      parse or raise_error
+    end
+
+    # Parse the entire source or register a parse failure
+    #
+    # @return the parse result if the entire source was matched
+    def parse_fully
+      (result = parse) && (@scanner.eos? || fail { :EOF }) && result
+    end
+
+    # Parse the entire source or raise a {SyntaxError}
+    #
+    # @raise [SyntaxError] a {SyntaxError} if the parse fails or the entire
+    #   source is not matched
+    #
+    # @return (see #parse_fully)
+    def parse_fully!
+      parse_full or raise_error
+    end
+
     # The current parse position
     # @return [Integer] the current parse position
     def pos
       @scanner.pos
     end
-    
+
     # Set the current parse position
     # @param [Integer] n the new parse position
     # @return [Integer] n
     def pos=(n)
       @scanner.pos = n
     end
-    
+
     # Fail and register a parse failure, unless a failure has already
     # occurred at the same or later position in the source.
     #
@@ -58,28 +101,41 @@ module Rattler::Runtime
         register_failure pos, (block_given? ? yield : nil)
       end
     end
-    
+
     # Fail and register a parse failure, unless a failure has already
     # occurred at a later position in the source.
     #
-    # @yieldreturn [String, Symbol] a failure message or rule name
+    # @yieldreturn (see #fail)
     #
-    # @see ParseFailure
-    #
-    # @return [false]
+    # @return (see #fail)
     def fail! # :yield:
       pos = @scanner.pos
       unless failure? and @failure_pos > pos
         register_failure pos, (block_given? ? yield : nil)
       end
     end
-    
+
+    # Fail the same as <tt>#fail</tt> but cause the entire parse to fail
+    # immediately.
+    #
+    # @yieldreturn (see #fail)
+    #
+    # @return (see #fail)
+    def fail_parse
+      if block_given?
+        fail! { yield }
+      else
+        fail!
+      end
+      throw :parse_failed
+    end
+
     # Return true if there is a parse failure
     # @return [Boolean] true if there is a parse failure
     def failure?
       !@failure_pos.nil?
     end
-    
+
     # Return the last parse failure
     # @return [ParseFailure] the last parse failure
     def failure
@@ -87,9 +143,9 @@ module Rattler::Runtime
         @__failure__ ||= ParseFailure.new(source, @failure_pos, @failure_msg)
       end
     end
-    
+
     protected
-    
+
     # Finish any necessary clean-up based on the final parse result.
     # @param final_result the final parse result
     # @return final_result
@@ -97,7 +153,7 @@ module Rattler::Runtime
       clear_failure if final_result
       final_result
     end
-    
+
     # Register a parse failure
     #
     # @param [Integer] position the position of the failure
@@ -110,20 +166,20 @@ module Rattler::Runtime
       @__failure__ = nil
       false
     end
-    
+
     # Clear the registered failure
     def clear_failure
       @failure_pos = nil
       @failure_msg = nil
       @__failure__ = nil
     end
-    
+
     # Raise a {SyntaxError} for the last parse failure
     # @raise [SyntaxError] a {SyntaxError} for the last parse failure
     # @return [nothing]
     def raise_error
       raise SyntaxError, failure.to_s
     end
-    
+
   end
 end
--- lib/rattler/runtime/recursive_descent_parser.rb
+++ lib/rattler/runtime/recursive_descent_parser.rb
@@ -19,17 +19,7 @@ module Rattler::Runtime
   class RecursiveDescentParser < Parser
     include ParserHelper
     include Rattler::Grammar::GrammarDSL
-    
-    # Parse +source+ by matching the start rule and raise a {SyntaxError} if
-    # the parse fails.
-    #
-    # @param (see #initialize)
-    # @raise (see #parse!)
-    # @return (see #parse!)
-    def self.parse!(source, options={})
-      self.new(source, options).parse!
-    end
-    
+
     # Create a new recursive descent parser to parse +source+.
     #
     # @param (see Parser#initialize)
@@ -39,26 +29,7 @@ module Rattler::Runtime
       super
       @rule_method_names = Hash.new {|h, name| h[name] = :"match_#{name}" }
     end
-    
-    # Parse by matching the rule returned by <tt>#start_rule</tt> or
-    # <tt>:start</tt> if <tt>#start_rule</tt> is not defined.
-    #
-    # @return the result of applying the start rule
-    def parse
-      catch(:parse_failed) { return finish(match(start_rule)) }
-      false
-    end
-    
-    # Parse by matching the start rule and raise a {SyntaxError} if the parse
-    # fails.
-    #
-    # @raise [SyntaxError] a {SyntaxError} if the parse fails
-    #
-    # @return the result of applying the start rule if successful
-    def parse!
-      parse or raise_error
-    end
-    
+
     # Apply a rule by dispatching to the method associated with +rule_name+
     # which is named by <tt>"match_#{rule_name}"<tt>, and if the match fails
     # register a parse failure.
@@ -69,17 +40,25 @@ module Rattler::Runtime
     def match(rule_name)
       apply(rule_name) or fail { rule_name }
     end
-    
+
     def method_missing(symbol, *args)
       (symbol == :start_rule) ? :start : super
     end
-    
+
     def respond_to?(symbol)
       super or (symbol == :start_rule)
     end
-    
+
     protected
-    
+
+    # Parse by matching the rule returned by <tt>#start_rule</tt> or
+    # <tt>:start</tt> if <tt>#start_rule</tt> is not defined.
+    #
+    # @return the result of applying the start rule
+    def __parse__
+      match start_rule
+    end
+
     # Apply a rule by dispatching to the method associated with the given rule
     # name, which is named by <tt>"match_#{rule_name}"<tt>. This method is
     # called by +match+ and should not be called directly.
@@ -91,16 +70,6 @@ module Rattler::Runtime
     def apply(rule_name)
       send @rule_method_names[rule_name]
     end
-    
-    # Fail the same as <tt>#fail</tt> but cause the entire parse to fail.
-    def fail_parse
-      if block_given?
-        fail! { yield }
-      else
-        fail!
-      end
-      throw :parse_failed
-    end
-    
+
   end
 end
