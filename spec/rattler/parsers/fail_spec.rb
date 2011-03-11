8f9ca51198460163faced45b56a2b262fc6ddd36
jarhart <jarhart@gmail.com>
2011-03-11 13:54:56 -0800

    fix wrong describe

--- spec/rattler/parsers/fail_spec.rb
+++ spec/rattler/parsers/fail_spec.rb
@@ -1,20 +1,22 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe Assert do
+include Rattler::Parsers
+
+describe Rattler::Parsers::Fail do
   include CombinatorParserSpecHelper
 
   subject { Fail[:expr, 'malformed expression'] }
-  
+
   describe '#parse' do
     it 'fails' do
       parsing('anything').should fail
     end
   end
-  
+
   describe '#capturing?' do
     it 'is false' do
       subject.should_not be_capturing
     end
   end
-  
+
 end
