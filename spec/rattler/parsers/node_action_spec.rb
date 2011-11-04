dbbe7131d6096cdadc574d57adc20e909ef4746d
Jason Arhart <jarhart@gmail.com>
2011-11-03 19:48:03 -0700

    fix node_action_spec.rb

--- spec/rattler/parsers/node_action_spec.rb
+++ spec/rattler/parsers/node_action_spec.rb
@@ -1,9 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
+include Rattler::Runtime
+
 describe NodeAction do
   include CombinatorParserSpecHelper
-
-  include Rattler::Runtime
 
   describe '#parse' do
 
