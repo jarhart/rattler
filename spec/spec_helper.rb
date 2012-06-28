d83d71963af4cbd763272b65b05c370721ed905c
Jason Arhart <jarhart@gmail.com>
2012-06-27 20:34:24 -0700

    minor cleanup of spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require 'rspec'
 require 'rattler'
 
@@ -8,5 +8,4 @@ require 'rattler'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  
 end
