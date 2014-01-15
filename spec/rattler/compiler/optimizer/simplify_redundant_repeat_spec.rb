fef0653660652048172a8535c169adc22013fa80
Jason Arhart <jarhart@gmail.com>
2014-01-15 11:59:03 -0800

    Increase test coverage for SimplyRedundantRepeat optimization

--- spec/rattler/compiler/optimizer/simplify_redundant_repeat_spec.rb
+++ spec/rattler/compiler/optimizer/simplify_redundant_repeat_spec.rb
@@ -8,39 +8,105 @@ describe SimplifyRedundantRepeat do
   let(:matching) { OptimizationContext[:type => :matching] }
   let(:capturing) { OptimizationContext[:type => :capturing] }
 
-  describe '#apply' do
+  describe '#apply', 'given a repeat of a repeat' do
 
     let(:outer) { Repeat[inner, *outer_bounds] }
     let(:inner) { Repeat[match, *inner_bounds] }
     let(:match) { Match[/a/]}
 
-    context 'given a repeat with one-or-more bounds of a repeat with one-or-more bounds' do
-
-      let(:outer_bounds) { [1, nil] }
-      let(:inner_bounds) { [1, nil] }
-
-      it 'returns a repeat with one-or-more bounds' do
-        subject.apply(outer, matching).should == Repeat[match, 1, nil]
-      end
-    end
-
-    context 'given a repeat with optional bounds of a repeat with optional bounds' do
-
-      let(:outer_bounds) { [0, 1] }
-      let(:inner_bounds) { [0, 1] }
-
-      it 'returns a repeat with one-or-more bounds' do
-        subject.apply(outer, matching).should == Repeat[match, 0, 1]
-      end
-    end
-
-    context 'given a repeat with zero-or-more bounds of a repeat' do
+    context 'where the outer repeat is zero-or-more' do
 
       let(:outer_bounds) { [0, nil] }
-      let(:inner_bounds) { [1, nil] }
 
-      it 'returns a repeat with zero-or-more bounds' do
-        subject.apply(outer, matching).should == Repeat[match, 0, nil]
+      context 'and the inner repeat is zero-or-more' do
+
+        let(:inner_bounds) { [0, nil] }
+
+        it 'returns a repeat with zero-or-more bounds' do
+          subject.apply(outer, matching).should == Repeat[match, 0, nil]
+        end
+      end
+
+      context 'and the inner repeat is one-or-more' do
+
+        let(:inner_bounds) { [1, nil] }
+
+        it 'returns a repeat with zero-or-more bounds' do
+          subject.apply(outer, matching).should == Repeat[match, 0, nil]
+        end
+      end
+
+      context 'and the inner repeat is optional' do
+
+        let(:inner_bounds) { [0, 1] }
+
+        it 'returns a repeat with zero-or-more bounds' do
+          subject.apply(outer, matching).should == Repeat[match, 0, nil]
+        end
+      end
+    end
+
+    context 'where the outer repeat is one-or-more' do
+
+      let(:outer_bounds) { [1, nil] }
+
+      context 'and the inner repeat is zero-or-more' do
+
+        let(:inner_bounds) { [0, nil] }
+
+        it 'returns a repeat with zero-or-more bounds' do
+          subject.apply(outer, matching).should == Repeat[match, 0, nil]
+        end
+      end
+
+      context 'and the inner repeat is one-or-more' do
+
+        let(:inner_bounds) { [1, nil] }
+
+        it 'returns a repeat with one-or-more bounds' do
+          subject.apply(outer, matching).should == Repeat[match, 1, nil]
+        end
+      end
+
+      context 'and the inner repeat is optional' do
+
+        let(:inner_bounds) { [0, 1] }
+
+        it 'returns a repeat with zero-or-more bounds' do
+          subject.apply(outer, matching).should == Repeat[match, 0, nil]
+        end
+      end
+    end
+
+    context 'where the outer repeat is optional' do
+
+      let(:outer_bounds) { [0, 1] }
+
+      context 'and the inner repeat is zero-or-more' do
+
+        let(:inner_bounds) { [0, nil] }
+
+        it 'returns a repeat with zero-or-more bounds' do
+          subject.apply(outer, matching).should == Repeat[match, 0, nil]
+        end
+      end
+
+      context 'and the inner repeat is one-or-more' do
+
+        let(:inner_bounds) { [1, nil] }
+
+        it 'returns a repeat with zero-or-more bounds' do
+          subject.apply(outer, matching).should == Repeat[match, 0, nil]
+        end
+      end
+
+      context 'and the inner repeat is optional' do
+
+        let(:inner_bounds) { [0, 1] }
+
+        it 'returns a repeat with optional bounds' do
+          subject.apply(outer, matching).should == Repeat[match, 0, 1]
+        end
       end
     end
   end
@@ -49,15 +115,33 @@ describe SimplifyRedundantRepeat do
 
     context 'given a repeat of a repeat' do
 
-      let(:parser) { Repeat[Repeat[Match[/a/], 1, nil], 0, nil] }
+      let(:parser) { Repeat[Repeat[Match[/a/], *inner_bounds], 0, nil] }
 
       context 'in the :matching context' do
-        it 'returns true' do
-          subject.applies_to?(parser, matching).should be_true
+
+        context 'if the inner repeat has zero-or-more bounds' do
+
+          let(:inner_bounds) { [0, nil] }
+
+          it 'returns true' do
+            subject.applies_to?(parser, matching).should be_true
+          end
+        end
+
+        context 'if the inner repeat has one-or-more bounds' do
+
+          let(:inner_bounds) { [1, nil] }
+
+          it 'returns true' do
+            subject.applies_to?(parser, matching).should be_true
+          end
         end
       end
 
       context 'in the :capturing context' do
+
+        let(:inner_bounds) { [0, nil] }
+
         it 'returns false' do
           subject.applies_to?(parser, capturing).should be_false
         end
